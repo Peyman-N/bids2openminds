418dd422b8a29d6de94e5ac04cdaefc269a0936c
Peyman-N <najafe.peyman@gmail.Com>
2024-12-16 02:38:41 +0100

    2024-12-16

--- bids2openminds/neuroimaging_mapping.py
+++ bids2openminds/neuroimaging_mapping.py
@@ -2,8 +2,70 @@ import openminds.latest.neuroimaging as neuroimaging
 import openminds.latest.controlled_terms as controlled_terms
 import openminds.latest.core as omcore
 
-def add_mri_scanner(mri_scanners,mri_scanner):
-    def eq(mri_scanner1, mri_scanner2):
+
+def extract_metadata(metadata, property):
+    if dict[property] in metadata:
+        return metadata[dict[property]]
+    else:
+        return None
+
+def create_mri_scanner(metadata, mri_scanners, collection, dataset_full_name):
+    def _create_mri_scanner(metadata, dataset_full_name):
+
+        if "Manufacturer" in metadata:
+            manufacturers = omcore.Organization(full_name=metadata["Manufacturer"])
+        else:
+            manufacturers = None
+
+        if "ManufacturersModelName" in metadata:
+            name = metadata["ManufacturersModelName"]
+        else:
+            name = f"MRI scanner used in {dataset_full_name}"
+
+        if "DeviceSerialNumber" in metadata:
+            serial_number = metadata["DeviceSerialNumber"]
+        else:
+            serial_number = None
+
+        if "StationName" in metadata:
+            lookup_label = metadata["StationName"]
+        else:
+            lookup_label = None
+
+        if "MagneticFieldStrength" in metadata:
+            magnetic_field_strength = omcore.QuantitativeValue(
+                value=float(metadata["MagneticFieldStrength"]),
+                unit=controlled_terms.UnitOfMeasurement.by_name("tesla"))
+
+        else:
+            magnetic_field_strength = None
+
+        if "InstitutionName" in metadata:
+            institution = omcore.Organization(full_name=metadata["InstitutionName"])
+            if "InstitutionalDepartmentName" in metadata:
+                department_name = f"{metadata["InstitutionName"]} department of {metadata["InstitutionName"]}"
+                owner = omcore.Organization(
+                    full_name=department_name,
+                    has_parents=institution)
+            else:
+                owner = institution
+        else:
+            owner = None
+
+        # TODO more information about MRI scanner can be added to description
+
+        mri_scanner = neuroimaging.MRIScanner(
+            manufacturers=manufacturers,
+            name=name,
+            serial_number=serial_number,
+            lookup_label=lookup_label,
+            magnetic_field_strength=magnetic_field_strength,
+            owner=owner
+        )
+
+        return mri_scanner
+    
+    def _eq(mri_scanner1, mri_scanner2):
 
         # If both have digital identifiers and they are same that means they are the same device
         if (mri_scanner1.digital_identifier is not None) and (mri_scanner2.digital_identifier is not None):
@@ -25,74 +87,31 @@ def add_mri_scanner(mri_scanners,mri_scanner):
                 return False
         return False
 
-def create_mri_scanner(metadata,mri_scanners,dataset_full_name):
+    new_mri_scanner = _create_mri_scanner(metadata, dataset_full_name)
+    
+    for scanner in mri_scanners:
+        if _eq(scanner, new_mri_scanner):
+            return scanner
+    
+    collection.add(new_mri_scanner)
+    mri_scanners.append(new_mri_scanner)
+    return new_mri_scanner
 
-    if "Manufacturer" in metadata:
-        manufacturers=omcore.Organization(full_name=metadata["Manufacturer"])
-    else:
-        manufacturers=None
-    
-    if "ManufacturersModelName" in metadata:
-        name=metadata["ManufacturersModelName"]
-    else:
-        name=f"MRI scanner used in {dataset_full_name}"
-    
-    if "DeviceSerialNumber" in metadata:
-        serial_number=metadata["DeviceSerialNumber"]
-    else:
-        serial_number=None
-    
-    if "StationName" in metadata:
-        lookup_label=metadata["StationName"]
-    else:
-        lookup_label=None
 
-    if "MagneticFieldStrength" in metadata:
-        magnetic_field_strength=omcore.QuantitativeValue(
-            value=float(metadata["MagneticFieldStrength"]),
-            unit=controlled_terms.UnitOfMeasurement.by_name("tesla"))
-
-    else:
-        magnetic_field_strength=None
-
-    if "InstitutionName" in metadata:
-        institution=omcore.Organization(full_name=metadata["InstitutionName"])
-        if "InstitutionalDepartmentName" in metadata:
-            department_name=f"{metadata["InstitutionName"]} department of {metadata["InstitutionName"]}"
-            owner=omcore.Organization(
-                full_name=department_name,
-                has_parents=institution)
-        else:
-            owner=institution
-    else:
-        owner=None
-
-    #TODO more information about MRI scanner can be added to description 
-    
-    mri_scanner=neuroimaging.MRIScanner(
-        manufacturers=manufacturers,
-        name=name,
-        serial_number=serial_number,
-        lookup_label=lookup_label,
-        magnetic_field_strength=magnetic_field_strength,
-        owner=owner
-    )
-    
-    return mri_scanner
-    
+def create_mri_scanner_usage(metadata, mri_scanner, dataset_full_name):
+    if 
 
 def create_functional_MRI_acquisition():
     return None
 
 
-
-def create_neuroimaging(bids_layout,collection,subject_dict):
-    mri_scanners=[],
+def create_neuroimaging(bids_layout, collection, subject_dict, dataset_full_name):
+    mri_scanners = [],
     nifti_files=bids_layout.get(extension=["nii.gz","nii"])
     for file in nifti_files:
         metadata=file.get_metadata()
         entities=file.get_entities()
-        mri_scanner = create_mri_scanner(metadata, mri_scanners)
+        mri_scanner = create_mri_scanner(metadata, mri_scanners, collection, dataset_full_name)
 
         if "session" in entities:
             session = entities["session"]
